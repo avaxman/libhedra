2e0069d318fcd78b820d996c7aeff41832b7e643
Amir Vaxman <amirvaxman@Amirs-MBP.lan>
2016-09-23 22:35:20 +0200

    concyclity written, not tested.

---
+++ include/hedra/concyclity.h
@@ -0,0 +1,62 @@
+// This file is part of libhedra, a library for polyhedral mesh processing
+//
+// Copyright (C) 2016 Amir Vaxman <avaxman@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+#ifndef HEDRA_CONCYCLITY_H
+#define HEDRA_CONCYCLITY_H
+#include <igl/igl_inline.h>
+#include <hedra/quat_cross_ratio.h>
+#include <Eigen/Core>
+#include <vector>
+#include <cmath> 
+
+
+namespace hedra
+{
+    // Computes the concyclity of a face, which is pi-argument of the cross-ratio. That is cos(angle)=-Re(cr)/norm(cr)
+    
+    // Inputs:
+    //  V           eigen double matrix     #V by 3 - mesh coordinates
+    //  D           eigen int matrix        #F by 1 - face degrees
+    //  F           eigen int matrix        #F by max(D)
+    // Outputs:
+    //  planarity   eigen double matix      #F by 1
+    IGL_INLINE bool concyclity(const Eigen::MatrixXd& V,
+                               const Eigen::VectorXi& D,
+                               const Eigen::MatrixXi& F,
+                               Eigen::VectorXd& concyclity)
+    {
+        using namespace Eigen;
+        concyclity.resize(D.size());
+        
+        Eigen::MatrixXi quadIndices(D.sum());
+        int currIndex=0;
+        for (int i=0;i<D.size();i++)
+            for (j=0;j<D(i);j++)
+                quadIndices.row(currIndex++)>>F(i,j),F(i,(j+1)%D(i)), F(i,(j+2)%D(i)), F(i,(j+3)%D(i));
+        
+        MatrixXd cr;
+        quat_cross_ratio(V,quadIndices, cr);
+        
+        VectorXd realPart=cr.col(0);
+        VectorXd absPart=cr.rowwise().norm();
+        VectorXd crAngles=acos(-realPart.cwiseQuotient(absPart).array());
+        
+        currIndex=0;
+        for (int i=0;i<D.rows();i++){
+            concyclity(i)=crAngles.segment(currIndex, currIndex+D(i));
+            currIndex+=D(i);
+        }
+        return true;
+    }
+}
+
+    
+
+
+#endif
+
+
