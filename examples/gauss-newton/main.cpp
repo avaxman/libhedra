4c49f54f796c9b023e9b2dfbb07f7b132b8d4233
Amir Vaxman <amirvaxman@amirs-imac.cs.uu.nl>
2016-10-01 19:01:04 +0200

    gauss-newton example empty.

---
+++ examples/gauss-newton/main.cpp
@@ -0,0 +1,14 @@
+#include <hedra/GNSolver.h>
+#include <hedra/EigenSolverWrapper.h>
+#include<Eigen/SparseCholesky>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+    using namespace Eigen;
+    
+    
+    return 0;
+}
+
+
