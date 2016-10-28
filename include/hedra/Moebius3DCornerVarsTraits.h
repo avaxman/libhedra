373ae3c80c472c5b7431c0c04565ef23bf421d5f
Amir Vaxman <amirvaxman@amirs-imac.cs.uu.nl>
2016-10-28 12:37:17 +0200

    Calibrating SmoothFactor

--- include/hedra/Moebius3DCornerVarsTraits.h
+++ include/hedra/Moebius3DCornerVarsTraits.h
@@ -437,7 +437,7 @@ namespace hedra { namespace optimization {
             double rate=constVec.lpNorm<Eigen::Infinity>()/prevError;
             double reduceRate=std::min(rate/2.0,1.0);
             
-            smoothFactor*=0.75;//-0.7*(1.0-reduceRate);
+            smoothFactor*=0.9;//-0.7*(1.0-reduceRate);
             std::cout<<"smoothFactor: "<<smoothFactor<<std::endl;
            
             
