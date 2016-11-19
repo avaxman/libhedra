3d3a2f0773b70a5ef671c7943daf8ecf2a3ae53d
Amir Vaxman <amirvaxman@e255-111.eduroam.tuwien.ac.at>
2016-11-19 17:25:51 +0100

    Wrote ShapeUp main file. Nothing is compiled or checked yet.

---
+++ include/hedra/shapeup.h
@@ -0,0 +1,144 @@
+// This file is part of libhedra, a library for polyhedral mesh processing
+//
+// Copyright (C) 2016 Amir Vaxman <avaxman@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+#ifndef HEDRA_SHAPE_UP_H
+#define HEDRA_SHAPE_UP_H
+
+#include <igl/igl_inline.h>
+#include <igl/setdiff.h>
+#include <igl/cat.h>
+#include <Eigen/Core>
+#include <Eigen/SimplicialLLT>
+#include <vector>
+
+
+//These functions implements the following algorithm:
+
+//Boaziz et al.
+//Shape-Up: Shaping Discrete Geometry with Projections
+//Computer Graphics Forum (Proc. SGP) 31(5), 2012
+
+namespace hedra
+{
+    
+    //This function does the precomputation of the necessary operators for the shape-up projection process.
+    
+    //input:
+    //  V  eigen double matrix  #V by 3 original mesh coordinates
+    //  D  eigen int vector     #F by 1 - face degrees
+    //  F  eigen int matrix     #F by max(D) - vertex indices in face
+    //  projection              function pointer taking (V,D,F,setsets),
+    //  where "subset" is a #S by n matrix of subsets of n vertices (columns of a row) that are projected into
+    //  the s \in S constraints (one constraint per row. The function returns a matrix of the projected vertex positions in xyzxyz format
+    //  (size #S by 3n).
+    //  h eigen int vector      #constrained vertex indices (handles)
+    //
+    
+    // Output:
+    // sudata struct ShapeupData     the data necessary to solve the system in shapeup_compute
+    
+    //TODO: Currently uniform weights. Make them geometric.
+    
+    struct ShapeupData{
+        
+        //input data
+        Eigen::MatrixXd& V,
+        Eigen::VectorXi& D,
+        Eigen::MatrixXi& F,
+        Eigen::VectorXi& SD,
+        Eigen::MatrixXi& S,
+        Eigen::VectorXi& h,
+        
+        //relevant matrices
+        Eigen::SparseMatrix<double> A, Q, C, E;
+        
+        Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > solver;
+        
+
+    };
+
+    IGL_INLINE void shapeup_precompute(const Eigen::MatrixXd& V,
+                                       const Eigen::VectorXi& D,
+                                       const Eigen::MatrixXi& F,
+                                       const Eigen::VectorXi& SD,
+                                       const Eigen::MatrixXi& S,
+                                       const Eigen::VectorXi& h,
+                                       const double shapeCoeff,
+                                       const double closeCoeff,
+                                       struct ShapeupData& sudata)
+    {
+        using namespace Eigen;
+        //The integration solve is separable to x,y,z components
+        sudata.V=V; sudata.F=F; sudata.D=D; sudata.SD=SD; sudata.S=S; sudata.h=h;
+        sudata.Q.conservativeResize(subsets.rows(), V.rows());  //Shape matrix (integration);
+        sudata.C.conservativeResize(h.rows(), V.rows());        //Closeness matrix for handles
+        
+        vector<Triplet<double> > QTriplets;
+        int currRow=0;
+        for (int i=0;i<S.rows();i++){
+            double avgCoeff=1.0/(double)SD(i);
+            for (int j=0;j<SD(i);j++){
+                for (int k=0;k<SD(i);k++){
+                    if (j==k)
+                        QTriplets.push_back(Triplet<double>(currRow+j, S(i,k), shapeCoeff*(1.0-avgCoeff)));
+                    else
+                        QTriplets.push_back(Triplet<double>(currRow+j, S(i,k), shapeCoeff*(-avgCoeff)));
+                }
+            }
+            currRow+=SD(i);
+        }
+        
+        sudata.Q.setFromTriplets(QTriplets.begin(), QTriplets.end());
+        
+        vector<Triplet<double> > CTriplets;
+        for (int i=0;i<h.size();i++)
+            CTriplets.push_back(Triplet<double>(i,h(i), closeCoeff));
+        
+        sudata.C.setFromTriplets(CTriplets.begin(), CTriplets.end());
+        
+        igl::cat(sudata.Q, sudata.C, sudata.A);
+        
+        sudata.E=sudata.A.transpose()*sudata.A();
+        sudata.solver.compute(sudata.E);
+    }
+    
+    
+    
+    IGL_INLINE void shapeup_compute(void (*projection) (const struct ShapeupData&, const Eigen::MatrixXd, const Eigen::MatrixXd&),
+                                    const Eigen::MatrixXd& vh,
+                                    const struct ShapeupData& sudata,
+                                    Eigen::MatrixXd& currV,
+                                    const int maxIterations=50;
+                                    const double& vTolerance=10e-6)
+    
+    {
+        using namespace Eigen;
+        MatrixXd prevV=currV;
+        MatrixXd PV;
+        MatrixXd b(sudata.A.rows(),3);
+        b.block(sudata.Q.rows(), 0, sudata.h.rows(),3)=vh;  //this stays constant throughout the iterations
+        for (int i=0;i<maxIterations;i++){
+            projection(sudata, currV, PV);
+            //constructing the projection part of the right hand side
+            int currRow=0;
+            for (int i=0;i<S.rows();i++){
+                for (int j=0;j<SD(i);j++)
+                    b.row(currRow)=PV.block(i, 3*j, 1,3).transpose();
+                currRow+=SD(i);
+            }
+            currV=solver.solve(sudata.A.transpose()*b);
+            double currChange=(currV-prevV).lpNorm<Infinity>;
+            std::cout<<"Iteration: "<<i<<", currChange: "<<currChange<<std::endl;
+            if (currChange<vTolerance)
+                break;
+            
+        }
+    }
+}
+
+
+#endif
