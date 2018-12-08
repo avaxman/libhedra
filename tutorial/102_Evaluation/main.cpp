bdda89ac2d3a51f163b1cd74b4a8faeba16e6ee0
Amir Vaxman <a.vaxman@uu.nl>
2018-12-08 21:57:53 +0900

    Modernized 102.

--- tutorial/102_Evaluation/main.cpp
+++ tutorial/102_Evaluation/main.cpp
@@ -6,118 +6,128 @@
 #include <hedra/planarity.h>
 #include <hedra/concyclity.h>
 #include <hedra/scalar2RGB.h>
+#include <hedra/polygonal_edge_lines.h>
 
 enum ViewingMode{PLANARITY, CONCYCLITY, EDGE_LENGTHS} ViewingMode;
 
-Eigen::MatrixXd V, edgeV;
-Eigen::MatrixXi F, T, midedgeT;
-Eigen::VectorXi D, TF, edgeTE;
-Eigen::MatrixXi EV, EF, FE, EFi, edgeT;
+Eigen::MatrixXd VMesh, VEdgeMesh, CMesh;
+Eigen::MatrixXd VEdges, CEdges;
+Eigen::MatrixXi F, TMesh, TEdges, TEdgeMesh;
+Eigen::VectorXi D, TF, TEEdgeMesh;
+Eigen::MatrixXi EV, EF, FE, EFi;
 Eigen::MatrixXd FEs;
 Eigen::VectorXi innerEdges;
-Eigen::Vector3d spans;
 Eigen::VectorXd planarity;
 Eigen::VectorXd concyclity;
 Eigen::VectorXd edgeLengths;
 double minEdgeLength, maxEdgeLength;
 
 
-bool UpdateCurrentView(igl::opengl::glfw::Viewer & viewer)
+bool update_meshes(igl::opengl::glfw::Viewer & viewer)
 {
-    using namespace Eigen;
-    using namespace std;
+  using namespace Eigen;
+  using namespace std;
+  
+  if ((ViewingMode==PLANARITY)||(ViewingMode==CONCYCLITY)){
+    Eigen::MatrixXd C;
+    if (ViewingMode==PLANARITY)
+      hedra::scalar2RGB(planarity, 0.0,1.0, C);
+    else
+      hedra::scalar2RGB(concyclity, 0.0,5.0, C);
     
-    Eigen::MatrixXd bigV;
-    Eigen::MatrixXi bigT;
-    Eigen::MatrixXd bigTC;
+    CMesh.conservativeResize(TMesh.rows(),3);
+    for (int i=0;i<TMesh.rows();i++)
+      CMesh.row(i)=C.row(TF(i));
     
-    if ((ViewingMode==PLANARITY)||(ViewingMode==CONCYCLITY)){
-        bigV=V;
-        bigT=T;
-        Eigen::MatrixXd C;
-        if (ViewingMode==PLANARITY)
-            hedra::scalar2RGB(planarity, 0.0,1.0, C);
-        else
-            hedra::scalar2RGB(concyclity, 0.0,5.0, C);
-        bigTC.resize(T.rows(),3);
-        for (int i=0;i<T.rows();i++)
-            bigTC.row(i)=C.row(TF(i));
-    }
+    viewer.data_list[0].clear();
+    viewer.data_list[0].set_mesh(VMesh, TMesh);
+    viewer.data_list[0].set_colors(CMesh);
+    viewer.data_list[0].show_lines=false;
     
-    if (ViewingMode==EDGE_LENGTHS){
-        bigV=edgeV;
-        bigT=edgeT;
-        Eigen::MatrixXd C;
-        hedra::scalar2RGB(edgeLengths, minEdgeLength,maxEdgeLength, C);
-        bigTC.resize(edgeT.rows(),3);
-        for (int i=0;i<edgeT.rows();i++)
-            bigTC.row(i)=C.row(edgeTE(i));
-    }
+  }
+  
+  if (ViewingMode==EDGE_LENGTHS){
+    Eigen::MatrixXd C;
+    hedra::scalar2RGB(edgeLengths, minEdgeLength,maxEdgeLength, C);
+    CMesh.conservativeResize(TEdgeMesh.rows(),3);
+    for (int i=0;i<TEdgeMesh.rows();i++)
+      CMesh.row(i)=C.row(TEEdgeMesh(i));
     
-    viewer.data().show_lines=false;
-    Eigen::MatrixXd OrigEdgeColors(EV.rows(),3);
-    OrigEdgeColors.col(0)=Eigen::VectorXd::Constant(EV.rows(),0.0);
-    OrigEdgeColors.col(1)=Eigen::VectorXd::Constant(EV.rows(),0.0);
-    OrigEdgeColors.col(2)=Eigen::VectorXd::Constant(EV.rows(),0.0);
+    viewer.data_list[0].clear();
+    viewer.data_list[0].set_mesh(VEdgeMesh, TEdgeMesh);
+    viewer.data_list[0].set_colors(CMesh);
+    viewer.data_list[0].show_lines=false;
     
-    viewer.data().clear();
-    viewer.data().set_mesh(bigV,bigT);
-    viewer.data().set_colors(bigTC);
-    viewer.data().compute_normals();
-    viewer.data().set_edges(V,EV,OrigEdgeColors);
-    return true;
+  }
+  
+  return true;
 }
 
 
 
 bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifiers)
 {
-    switch(key)
-    {
-        case '1': ViewingMode=PLANARITY; break;
-        case '2': ViewingMode=CONCYCLITY; break;
-        case '3': ViewingMode=EDGE_LENGTHS; break;
-    }
-    UpdateCurrentView(viewer);
-    return false;
+  switch(key)
+  {
+    case '1': ViewingMode=PLANARITY; break;
+    case '2': ViewingMode=CONCYCLITY; break;
+    case '3': ViewingMode=EDGE_LENGTHS; break;
+  }
+  update_meshes(viewer);
+  return false;
 }
 
 
 int main(int argc, char *argv[])
 {
-    
-    // Load a mesh in OFF format
-    using namespace std;
-    using namespace Eigen;
-    
-    std::cout<<R"(
-    1 Show planarity between [0,1]
-    2 Show concyclity between [0,5]
-    3 Show normalized edge lengths
-    )";
-    
-    hedra::polygonal_read_OFF(TUTORIAL_SHARED_PATH "/intersection.off", V, D, F);
-    hedra::polygonal_edge_topology(D, F, EV, FE, EF,EFi,FEs,innerEdges);
-    hedra::triangulate_mesh(D, F, T, TF);
-    hedra::edge_mesh(V,D,F,EV, EF, edgeV, edgeT,edgeTE);
-    spans=V.colwise().maxCoeff()-V.colwise().minCoeff();
-    
-    hedra::planarity(V,D,F,planarity);
-    hedra::concyclity(V,D,F,concyclity);
-    
-    edgeLengths.resize(EV.rows());
-    for (int i=0;i<EV.rows();i++)
-        edgeLengths(i)=(V.row(EV(i,0))-V.row(EV(i,1))).norm();
-    
-    minEdgeLength=edgeLengths.minCoeff();
-    maxEdgeLength=edgeLengths.maxCoeff();
-    
-    ViewingMode=PLANARITY;
-    igl::opengl::glfw::Viewer viewer;
-    viewer.callback_key_down=&key_down;
-    viewer.core.background_color<<0.75,0.75,0.75,1.0;
-    UpdateCurrentView(viewer);
-    viewer.launch();
-    
-
+  
+  // Load a mesh in OFF format
+  using namespace std;
+  using namespace Eigen;
+  
+  std::cout<<R"(
+  1 Visualize planarity between [0,1]
+  2 Visualize concyclity between [0,5]
+  3 Visualize normalized edge lengths
+  )";
+  
+  hedra::polygonal_read_OFF(TUTORIAL_SHARED_PATH "/intersection.off", VMesh, D, F);
+  hedra::polygonal_edge_topology(D, F, EV, FE, EF,EFi,FEs,innerEdges);
+  hedra::triangulate_mesh(D, F, TMesh, TF);
+  hedra::edge_mesh(VMesh,D,F,EV, EF, VEdgeMesh, TEdgeMesh,TEEdgeMesh);
+  
+  hedra::planarity(VMesh,D,F,planarity);
+  hedra::concyclity(VMesh,D,F,concyclity);
+  
+  edgeLengths.resize(EV.rows());
+  for (int i=0;i<EV.rows();i++)
+    edgeLengths(i)=(VMesh.row(EV(i,0))-VMesh.row(EV(i,1))).norm();
+  
+  minEdgeLength=edgeLengths.minCoeff();
+  maxEdgeLength=edgeLengths.maxCoeff();
+  
+  igl::opengl::glfw::Viewer viewer;
+  
+  //Polygonal triangulated mesh
+  viewer.data_list[0].clear();
+  viewer.data_list[0].set_mesh(VMesh,TMesh);
+  viewer.data_list[0].set_face_based(true);
+  viewer.data_list[0].show_lines=false;
+  viewer.data_list[0].invert_normals=true;
+  
+  //edges mesh
+  viewer.append_mesh();
+  hedra::polygonal_edge_lines(VMesh, F, TMesh, EV,  VEdges, TEdges, CEdges,0.25);
+  viewer.data_list[1].set_mesh(VEdges, TEdges);
+  viewer.data_list[1].set_colors(CEdges);
+  viewer.data_list[1].show_lines=false;
+  
+  viewer.selected_data_index=0;
+  
+  ViewingMode=PLANARITY;
+  viewer.callback_key_down=&key_down;
+  viewer.core.background_color<<0.75,0.75,0.75,1.0;
+  update_meshes(viewer);
+  viewer.launch();
+  
 }
